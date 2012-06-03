78f5e65db35428f58aac3f6ceaa020b6a7e68892
Kanji Furuhashi <pikanji@gmail.com>
2012-06-03 05:17:07 +0000

    not much

--- app.js
+++ app.js
@@ -42,14 +42,16 @@ app.get('/test', function(req, res){
 
     var data = {
         name: "Test Page!",
-        home: "a small planet somewhere in the vicinity of Betelgeuse"
+        home: "a small planet somewhere in the vicinity of Betelgeuse",
+	uri: encodeURI('http://ec2-184-73-143-220.compute-1.amazonaws.com:59634/')
     };
+    
 
     res.render('index.html', data);
 });
 
 
 //app.listen(3000);
-app.listen(3000, function(){
+app.listen(59634, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
